2c45de99e449d70a7e69ecb797c7b9b03389a2e5
Bela <belahamid@gmail.com>
2020-12-29 13:33:59 +0000

    added time_taken()

--- task_list.py
+++ task_list.py
@@ -6,45 +6,48 @@ tasks = [
     { "description": "Walk Dog", "completed": True, "time_taken": 60 },
 ]
 
-
-# As a user, to manage my task list I would like a program that allows me to:
-
-# Print a list of uncompleted tasks - filter by true or false
-# for task in tasks:
-#     if task["completed"] == False:
-#         print(task)
-
-def uncompleted(tasks):
+#Print a list of uncompleted tasks
+def uncompleted_tasks(tasks):
     uncompleted = []
     for task in tasks:
         if task["completed"] == False:
             uncompleted.append(task)
-    return uncompleted
+            print(task["description"])
+    print(uncompleted)
 
-print(uncompleted(tasks))
+uncompleted_tasks(tasks)
 
-# Print a list of completed tasks - filter by true or false
+# Print a list of completed tasks -
 
 def completed(tasks):
-    uncompleted = []
+    completed = []
     for task in tasks:
         if task["completed"] == True:
-            uncompleted.append(task)
-    return uncompleted
+            completed.append(task)
+            print(task["description"])
+    return completed
 
 print("Completed")
-print(completed(tasks))
+completed(tasks)
 
-# for task in tasks:
-#     if task["completed"] == True:
-#         print(task)
 
 # Print a list of all task descriptions - 
 
-# for task in tasks:
-#     print (task["description"])
+def task_descriptions(tasks):
+    for task in tasks:
+        print(task["description"])
+
+print("TASK DESCRIPTIONS")
+task_descriptions(tasks)
 
 # Print a list of tasks where time_taken is at least a given time - print (if time_taken >= 0)
+def time_taken(tasks, time):
+    for task in tasks:
+        if task["time_taken"] > time:
+            print(task)
+
+print("TIME TAKEN TASKS")
+time_taken(tasks, 20)
 
 # for task in tasks:
 #     if task["time_taken"] < 20:
@@ -52,6 +55,6 @@ print(completed(tasks))
 
 # Print any task with a given description 
 
-for task in tasks:
-    if task["description"] == 'Wash Dishes':
-        print(task)
+# for task in tasks:
+#     if task["description"] == 'Wash Dishes':
+#         print(task)
